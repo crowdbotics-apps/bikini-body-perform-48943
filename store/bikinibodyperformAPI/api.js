e530ef73fed9b0e9262ee284f5ebf0bd39f6271e
Crowdbotics <engineering@crowdbotics.com>
2024-08-03 23:15:28 +0000

    Update store.

--- store/bikinibodyperformAPI/api.js
+++ store/bikinibodyperformAPI/api.js
@@ -14,6 +14,105 @@ function api_v1_login_create(payload) {
 function api_v1_signup_create(payload) {
   return bikinibodyperformAPI.post(`/api/v1/signup/`, payload)
 }
+function modules_appointment_local_appointment_list(payload) {
+  return bikinibodyperformAPI.get(`/modules/appointment-local/appointment/`)
+}
+function modules_appointment_local_appointment_create(payload) {
+  return bikinibodyperformAPI.post(
+    `/modules/appointment-local/appointment/`,
+    payload
+  )
+}
+function modules_appointment_local_appointment_retrieve(payload) {
+  return bikinibodyperformAPI.get(
+    `/modules/appointment-local/appointment/${payload.id}/`
+  )
+}
+function modules_appointment_local_appointment_update(payload) {
+  return bikinibodyperformAPI.put(
+    `/modules/appointment-local/appointment/${payload.id}/`,
+    payload
+  )
+}
+function modules_appointment_local_appointment_partial_update(payload) {
+  return bikinibodyperformAPI.patch(
+    `/modules/appointment-local/appointment/${payload.id}/`,
+    payload
+  )
+}
+function modules_appointment_local_appointment_destroy(payload) {
+  return bikinibodyperformAPI.delete(
+    `/modules/appointment-local/appointment/${payload.id}/`
+  )
+}
+function modules_appointment_local_appointment_session_list(payload) {
+  return bikinibodyperformAPI.get(
+    `/modules/appointment-local/appointment_session/`
+  )
+}
+function modules_appointment_local_appointment_session_create(payload) {
+  return bikinibodyperformAPI.post(
+    `/modules/appointment-local/appointment_session/`,
+    payload
+  )
+}
+function modules_appointment_local_appointment_session_retrieve(payload) {
+  return bikinibodyperformAPI.get(
+    `/modules/appointment-local/appointment_session/${payload.id}/`
+  )
+}
+function modules_appointment_local_appointment_session_update(payload) {
+  return bikinibodyperformAPI.put(
+    `/modules/appointment-local/appointment_session/${payload.id}/`,
+    payload
+  )
+}
+function modules_appointment_local_appointment_session_partial_update(payload) {
+  return bikinibodyperformAPI.patch(
+    `/modules/appointment-local/appointment_session/${payload.id}/`,
+    payload
+  )
+}
+function modules_appointment_local_appointment_session_destroy(payload) {
+  return bikinibodyperformAPI.delete(
+    `/modules/appointment-local/appointment_session/${payload.id}/`
+  )
+}
+function modules_appointment_local_meetings_information_list(payload) {
+  return bikinibodyperformAPI.get(
+    `/modules/appointment-local/meetings-information/`
+  )
+}
+function modules_appointment_local_meetings_information_create(payload) {
+  return bikinibodyperformAPI.post(
+    `/modules/appointment-local/meetings-information/`,
+    payload
+  )
+}
+function modules_appointment_local_meetings_information_retrieve(payload) {
+  return bikinibodyperformAPI.get(
+    `/modules/appointment-local/meetings-information/${payload.id}/`
+  )
+}
+function modules_appointment_local_meetings_information_update(payload) {
+  return bikinibodyperformAPI.put(
+    `/modules/appointment-local/meetings-information/${payload.id}/`,
+    payload
+  )
+}
+function modules_appointment_local_meetings_information_partial_update(
+  payload
+) {
+  return bikinibodyperformAPI.patch(
+    `/modules/appointment-local/meetings-information/${payload.id}/`,
+    payload
+  )
+}
+function modules_appointment_local_meetings_information_destroy(payload) {
+  return bikinibodyperformAPI.delete(
+    `/modules/appointment-local/meetings-information/${payload.id}/`
+  )
+}
 function modules_appointment_service_appointment_remove_destroy(payload) {
   return bikinibodyperformAPI.delete(
     `/modules/appointment/service/${payload.id}/appointment/remove/`
@@ -92,6 +191,24 @@ export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
+  modules_appointment_local_appointment_list,
+  modules_appointment_local_appointment_create,
+  modules_appointment_local_appointment_retrieve,
+  modules_appointment_local_appointment_update,
+  modules_appointment_local_appointment_partial_update,
+  modules_appointment_local_appointment_destroy,
+  modules_appointment_local_appointment_session_list,
+  modules_appointment_local_appointment_session_create,
+  modules_appointment_local_appointment_session_retrieve,
+  modules_appointment_local_appointment_session_update,
+  modules_appointment_local_appointment_session_partial_update,
+  modules_appointment_local_appointment_session_destroy,
+  modules_appointment_local_meetings_information_list,
+  modules_appointment_local_meetings_information_create,
+  modules_appointment_local_meetings_information_retrieve,
+  modules_appointment_local_meetings_information_update,
+  modules_appointment_local_meetings_information_partial_update,
+  modules_appointment_local_meetings_information_destroy,
   modules_appointment_service_appointment_remove_destroy,
   modules_appointment_service_appointment_single_retrieve,
   modules_appointment_service_appointment_create_create,
