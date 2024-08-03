c89f3b8b70ed93de7dc42d496654e446bc1f76af
Crowdbotics <team@crowdbotics.com>
2024-08-03 21:44:50 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bikini-body-perform-48943",
+  url: "https://bikini-body-perform-48943.botics.co",
+  api: "https://bikini-body-perform-48943.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
